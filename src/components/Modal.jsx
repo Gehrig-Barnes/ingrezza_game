786c2540e8ff64eca5992ccfbe861990d51d1769
gehrig_barnes <gehrigbarnes@gmail.com>
2023-06-05 17:23:50 -0500

    done with modal pop up, now I just have to add some functionality to the questions

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,7 +3,9 @@ function Modal({ closeModal }) {
   return (
     <div id="modal">
       <div id="modalHeader">
-        <h2 id="modalHeadText">Take the TD Challenge: Assessment and diagnosis</h2>
+        <h2 id="modalHeadText">
+          Take the TD Challenge: Assessment and diagnosis
+        </h2>
         <span className="close" onClick={closeModal}>
           &times;
         </span>
@@ -11,8 +13,27 @@ function Modal({ closeModal }) {
       <div className="modal-overlay" onClick={closeModal}></div>
       <div className="modal-wrapper">
         <div className="modal-content">
-          <h2>Modal Title</h2>
-          <p>This is the content of the modal.</p>
+          <div className="grid-container">
+            <img
+              src="https://media.istockphoto.com/id/1297460438/vector/funny-comic-cartoon-brain-character-with-magnifier.jpg?s=612x612&w=0&k=20&c=gbQswIYSDpCOpdnbp9nkTHczmer0rNAINOIeZoHx7zM="
+              alt="brain_image"
+              id="quizImage"
+            />
+            <div className="question">
+              <p id="whichQuestion">Question 1 of 5</p>
+              <p id="tf"><b>TRUE OR FALSE:</b></p>
+              <p id="quest">The movements in this video show tardive dyskinesia (TD).</p>
+            </div>
+            <div id="trueOrFalse">
+              <button id="true"><b>TRUE</b></button>
+              <button id="false"><b>FALSE</b></button>
+            </div>
+            <img
+              src="https://i.makeagif.com/media/2-15-2023/ww83wG.gif"
+              alt="question_image"
+              id="gifImage"
+            />
+          </div>
         </div>
       </div>
     </div>
---
+++ src/questionData.js
@@ -0,0 +1,36 @@
+const questions = [
+  {
+    id: 1,
+    question: "The movements in this video show tardive dyskinesia (TD)",
+    answer: 'false',
+    gif: "https://i.makeagif.com/media/2-15-2023/ww83wG.gif",
+   
+  },
+  {
+    id: 2,
+    question: "The movements in this video show tardive dyskinesia (TD)",
+    answer: 'true',
+    gif: "https://i.makeagif.com/media/2-15-2023/ww83wG.gif",
+  },
+  {
+    id: 3,
+    question: "The movements in this video show tardive dyskinesia (TD)",
+    answer: 'false',
+    gif: "https://i.makeagif.com/media/2-15-2023/ww83wG.gif",
+  },
+  {
+    id: 4,
+    question: "The movements in this video show tardive dyskinesia (TD)",
+    answer: 'false',
+    gif: "https://i.makeagif.com/media/2-15-2023/ww83wG.gif",
+  },
+  {
+    id: 5,
+    question: "The movements in this video show tardive dyskinesia (TD)",
+    answer: 'true',
+    gif: "https://i.makeagif.com/media/2-15-2023/ww83wG.gif",
+  },
+  
+];
+
+export default questions;
