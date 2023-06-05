3febbfd03f055f42b1629bc7ec8fb62a82bcac02
gehrig_barnes <gehrigbarnes@gmail.com>
2023-06-05 11:04:09 -0500

    added logo to the circle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div id="box">
-        <div id="logo"></div>
+        <div id="logo">
+          <img
+            id="brain"
+            src="https://media.istockphoto.com/id/1297460438/vector/funny-comic-cartoon-brain-character-with-magnifier.jpg?s=612x612&w=0&k=20&c=gbQswIYSDpCOpdnbp9nkTHczmer0rNAINOIeZoHx7zM="
+            alt="brain_image"
+          />
+        </div>
+        <h3 id="title">Take the TD Challenge: Assessment and diagnosis</h3>
+        <h1 id="">
+          Can you differentiate tardive dyskinesia from drug-induced
+          parkinsonism and other acute drug-induced movement disorders? Put your
+          knowledge to the test.
+        </h1>
       </div>
     </div>
   );
