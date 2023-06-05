7d902d05de7494bf9929fe059f4e414330f397de
gehrig_barnes <gehrigbarnes@gmail.com>
2023-06-05 15:15:03 -0500

    header complete

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,12 +2,14 @@ import React from "react";
 function Modal({ closeModal }) {
   return (
     <div id="modal">
+      <div id="modalHeader">
+        <span className="close" onClick={closeModal}>
+          &times;
+        </span>
+      </div>
       <div className="modal-overlay" onClick={closeModal}></div>
       <div className="modal-wrapper">
         <div className="modal-content">
-          <span className="close" onClick={closeModal}>
-            &times;
-          </span>
           <h2>Modal Title</h2>
           <p>This is the content of the modal.</p>
         </div>
