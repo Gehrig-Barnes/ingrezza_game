d835e5789be029f98f7d9da35af40d42fa93c597
gehrig_barnes <gehrigbarnes@gmail.com>
2023-06-05 12:23:20 -0500

    so far so good, trying to figure out how to not get the bolded part to break from the p tag when I make it strong.

--- src/App.js
+++ src/App.js
@@ -11,12 +11,16 @@ function App() {
             alt="brain_image"
           />
         </div>
-        <h3 id="title">Take the TD Challenge: Assessment and diagnosis</h3>
-        <h1 id="">
-          Can you differentiate tardive dyskinesia from drug-induced
-          parkinsonism and other acute drug-induced movement disorders? Put your
-          knowledge to the test.
-        </h1>
+        <div id="container">
+          <h2 id="title">
+            <strong>Take the TD Challenge: Assessment and diagnosis</strong>
+          </h2>
+          <p id="description">
+            Can you differentiate tardive dyskinesia from drug-induced
+            parkinsonism and other acute drug-induced movement disorders?{" "}
+            <strong>Put your knowledge to the test.</strong>
+          </p>
+        </div>
       </div>
     </div>
   );
