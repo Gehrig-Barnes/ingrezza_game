ea2d3b39955eb7b00c812d96c6c68265c63a977a
gehrig_barnes <gehrigbarnes@gmail.com>
2023-06-06 11:37:49 -0500

    buttons show x or check mark if the answer is correct or wrong

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,41 @@
 import React, { useState } from "react";
 import questionData from "../questionData.js";
+import { isDisabled } from "@testing-library/user-event/dist/utils";
 function Modal({ closeModal }) {
   const [data, setData] = useState(questionData);
   const [questIndex, setQuestIndex] = useState(0);
   const [next, setNext] = useState(false);
+  const [total, setTotal] = useState(0);
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [falseFeedBack, setFalseFeedBack] = useState("FALSE");
+  const [trueFeedBack, setTrueFeedBack] = useState("TRUE");
 
-  function setNextToTrue() {
+  function handleTrueClick(e) {
+    const value = e.target.value;
+    const wrongTrue = "TRUE X";
+    const correctTrue = "TRUE ✔";
+
+    if (data[questIndex].answer === value) {
+      setTrueFeedBack(correctTrue);
+    } else {
+      setTrueFeedBack(wrongTrue);
+    }
+    setIsDisabled(true);
+    if (next === false) {
+      setNext(true);
+    }
+  }
+
+  function handleFalseClick(e) {
+    const value = e.target.value;
+    const wrongFalse = "FALSE X";
+    const correctFalse = "FALSE ✔";
+    if (data[questIndex].answer === value) {
+      setFalseFeedBack(correctFalse);
+    } else {
+      setFalseFeedBack(wrongFalse);
+    }
+    setIsDisabled(true);
     if (next === false) {
       setNext(true);
     }
@@ -13,6 +43,9 @@ function Modal({ closeModal }) {
 
   function setNextToFalse() {
     if (questIndex === 4) return;
+    setIsDisabled(false);
+    setFalseFeedBack("FALSE");
+    setTrueFeedBack("TRUE");
     if (next === true) {
       setNext(false);
     }
@@ -25,7 +58,7 @@ function Modal({ closeModal }) {
           Take the TD Challenge: Assessment and diagnosis
         </h2>
         {next ? (
-          <button onClick={setNextToFalse}id="next">
+          <button onClick={setNextToFalse} id="next">
             <b>NEXT QUESTION</b>
           </button>
         ) : null}
@@ -39,10 +72,10 @@ function Modal({ closeModal }) {
           <div className="grid-container">
             <div id="logo_box">
               <img
-              src="https://media.istockphoto.com/id/1297460438/vector/funny-comic-cartoon-brain-character-with-magnifier.jpg?s=612x612&w=0&k=20&c=gbQswIYSDpCOpdnbp9nkTHczmer0rNAINOIeZoHx7zM="
-              alt="brain_image"
-              id="quizImage"
-            />
+                src="https://media.istockphoto.com/id/1297460438/vector/funny-comic-cartoon-brain-character-with-magnifier.jpg?s=612x612&w=0&k=20&c=gbQswIYSDpCOpdnbp9nkTHczmer0rNAINOIeZoHx7zM="
+                alt="brain_image"
+                id="quizImage"
+              />
             </div>
             <div className="question">
               <p id="whichQuestion">Question {questIndex + 1} of 5</p>
@@ -52,19 +85,29 @@ function Modal({ closeModal }) {
               <p id="quest">{data[questIndex].question}</p>
             </div>
             <div id="trueOrFalse">
-              <button onClick={setNextToTrue}id="true">
-                <b>TRUE</b>
+              <button
+                onClick={handleTrueClick}
+                id="true"
+                value="true"
+                disabled={isDisabled}
+              >
+                {trueFeedBack}
               </button>
-              <button onClick={setNextToTrue}id="false">
-                <b>FALSE</b>
+              <button
+                onClick={handleFalseClick}
+                id="false"
+                value="false"
+                disabled={isDisabled}
+              >
+                {falseFeedBack}
               </button>
             </div>
             <div id="gifContainer">
               <img
-              src="https://i.makeagif.com/media/2-15-2023/ww83wG.gif"
-              alt="question_image"
-              id="gifImage"
-            />
+                src={data[questIndex].gif}
+                alt="question_image"
+                id="gifImage"
+              />
             </div>
           </div>
         </div>
--- src/questionData.js
+++ src/questionData.js
@@ -10,25 +10,25 @@ const questions = [
     id: 2,
     question: "The movements in this video show tardive dyskinesia (TD).",
     answer: 'true',
-    gif: "https://i.makeagif.com/media/2-15-2023/ww83wG.gif",
+    gif: "https://i.makeagif.com/media/2-13-2014/d_6agD.gif",
   },
   {
     id: 3,
     question: "Tardive dyskinesia (TD) and acute drug-induced movement disorders (eg, drug-induced parkinsonism) never coexist.",
     answer: 'false',
-    gif: "https://i.makeagif.com/media/2-15-2023/ww83wG.gif",
+    gif: "https://marketingweek.imgix.net/content/uploads/2021/09/17161907/Question-mark-2.jpg",
   },
   {
     id: 4,
     question: "The movements in this video show tardive dyskinesia (TD).",
     answer: 'false',
-    gif: "https://i.makeagif.com/media/2-15-2023/ww83wG.gif",
+    gif: "https://media.tenor.com/-UrD8zExORAAAAAC/tremor-hands.gif",
   },
   {
     id: 5,
     question: "The movements in this video show tardive dyskinesia (TD).",
     answer: 'true',
-    gif: "https://i.makeagif.com/media/2-15-2023/ww83wG.gif",
+    gif: "https://i.makeagif.com/media/4-10-2016/EuhHag.gif",
   },
   
 ];
