1659c2053f192ea6c8824a34b88a01b032e1a45a
gehrig_barnes <gehrigbarnes@gmail.com>
2023-06-07 10:46:05 -0500

    done! it's not perfect. There are still some css things that need to be adjusted to make it accesbile to different screen sizes. but for now, it's fine on my screen size. I'm happy with this mvp.

--- src/components/Menue/Modal/Score/Score.jsx
+++ src/components/Menue/Modal/Score/Score.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Score({ closeModal,total, circleId }) {
+function Score({ closeModal, total, circleId }) {
   return (
     <>
       <div id="modalHeader">
@@ -8,26 +8,33 @@ function Score({ closeModal,total, circleId }) {
           &times;
         </span>
       </div>
+      <h1 id="completed">You’ve completed the TD Challenge: Assessment and diagnosis</h1>
+      <button id="exit"><b>EXIT QUIZ</b></button>
       <div id="scoreCircle">
         <div class="innerCircle">
-          
+          <h1 id="final_score">{total}/5 correct</h1>
         </div>
-
       </div>
       <svg
-            xmlns="http://www.w3.org/2000/svg"
-            version="1.1"
-            width="300px"
-            height="300px"
-          >
-            <defs>
-              <linearGradient id="GradientColor">
-                <stop offset="0%" stop-color="#e91e63" />
-                <stop offset="100%" stop-color="#673ab7" />
-              </linearGradient>
-            </defs>
-            <circle id={circleId} cx="150" cy="150" r="112.5" stroke-linecap="round" />
-          </svg>
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        width="300px"
+        height="300px"
+      >
+        <defs>
+          <linearGradient id="GradientColor">
+            <stop offset="0%" stop-color="#fcfafb" />
+            <stop offset="100%" stop-color="#fcfafb" />
+          </linearGradient>
+        </defs>
+        <circle
+          id={circleId}
+          cx="150"
+          cy="150"
+          r="112.5"
+          stroke-linecap="round"
+        />
+      </svg>
     </>
   );
 }
